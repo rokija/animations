e12adeb61fffa37f0b0b5812bf472e6deba70d98
Ruta <rutafreimane14@gmail.com>
2018-11-24 16:07:24 +0100

    removed div from transition group

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router, Route, Switch, Redirect,
+  BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
-import Transitions from './transitions';
-
 
 import Home from './components/Home';
 import About from './components/About';
 import Details from './components/Details';
 import TabBar from './components/TabBar';
-
-import SlideUpAndDown from './animations/SlideUpAndDown';
 
 import './styles/base.less';
 
@@ -27,6 +22,7 @@ const App = () => (
         render={({ location }) => (
           <div style={{ height: '100%' }}>
             <TransitionGroup
+              component={null}
               childFactory={child => React.cloneElement(
                 child,
                 {
@@ -60,27 +56,5 @@ const App = () => (
     </React.Fragment>
   </Router>
 );
-
-// /* eslint-disable react/no-children-prop */
-// const App = () => (
-//   <Router>
-//     <React.Fragment>
-//       <Route
-//         render={({ location }) => (
-//           <div style={{ height: '100%' }}>
-//             <Transitions pageKey={location.key} {...location.state}>
-//               <Switch location={location}>
-//                 <Route path="/about" component={About} />
-//                 <Route path="/details" component={Details} />
-//                 <Redirect from="/" to="/about" />
-//               </Switch>
-//             </Transitions>
-//           </div>
-//         )}
-//       />
-//       <TabBar />
-//     </React.Fragment>
-//   </Router>
-// );
 
 ReactDOM.render(<App />, document.getElementById('root'));
